fc8798e831431d0007b797d0e54089289c0db9bb
Ghoustdownunder <taylorradke98@gmail.com>
2018-08-24 23:24:09 +1000

    Removed node_object class, replaced with dict. Fixed Reader. Solver is now greedy 2-opt

--- src/TSP.py
+++ src/TSP.py
@@ -1,26 +1,26 @@
 import sys
 import os.path as path
-from lib.file_handler import create_node_object
-from lib.solver import greedy3opt
-from lib.tsp_plot import build_plot
+from lib.reader import reader
+from lib.solver import greedy2opt
+
 
 def main(tsp_file_name,tsp_allowed_time): 
 
     tsp_allowed_time = tsp_allowed_time
 
     tsp_file_path = path.join('tsp_files',tsp_file_name)
-    tsp_node_obj = create_node_object(tsp_file_path)
+    tsp_node_obj = reader(tsp_file_path)
 
-    shortest = greedy3opt(tsp_node_obj.get_nodes(),tsp_allowed_time)
-    build_plot(shortest)
-    # print(tsp_file_name)
-    # print("Shortest found tour length: " + str(shortest[0]))
-    # print("Tour: ")
-    # for route in shortest[1][:-1]:
-    #     print(route[0])
-    # print(shortest[1][-1])
-    
+    result = greedy2opt(tsp_node_obj["tour"], tsp_allowed_time)
+
+    print(tsp_file_name)
+    print("Shortest Found Tour Length: " + str(result[0]))
+    print("Tour: ")
+    for node in result[1]:
+         print(node)
+    print(-1)
+
 
 if __name__ == "__main__":
     # main(sys.argv[1],sys.argv[2])
-    main("d18512.tsp",20)
+    main("berlin52.tsp",300)
--- src/lib/solver.py
+++ src/lib/solver.py
@@ -1,18 +1,22 @@
 import math
 from time import time
 
-def euclidean_distance(node1,node2):
+
+def euclidean_distance(node1, node2):
     """Finds the euclidean distance between two nodes"""
-    
+
     xd = abs(node1[1] - node2[1])
     yd = abs(node1[2] - node2[2])
     return math.sqrt(xd**2 + yd ** 2)
 
+
 def euclidean_sum(a):
-    a_sum = 0
+    sum = 0
     for i in range(len(a)-1):
-        a_sum += euclidean_distance(a[i],a[i+1])
-    return a_sum
+        sum += euclidean_distance(a[i], a[i+1])
+    sum += euclidean_distance(a[0], a[-1])
+    return sum
+
 
 def find_next_shortest(node_list):
     """Finds the next closest node"""
@@ -45,43 +49,45 @@ def greedy(node_list):
     tour_length += euclidean_distance(node_list[0],node_list[1])
     tour_list.append(node_list[0])
 
-    return tour_length,tour_list
-
-def swap(node_list,i,j):
-
-    temp = node_list[i]
-    node_list[i] = node_list[j]
-    node_list[j] = temp
+    return tour_list
 
 
-def greedy3opt(node_list,allowed_time):
+def opt_swap_2(route,i,k):
 
+    new_tour = []
+    new_tour[0:i-1] = [route[j] for j in range(0, i)]
+    new_tour[i:k-1] = reversed(route[i:k])
+    new_tour[k:] = route[k:]
+
+    return new_tour
+
+
+def opt2(route):
+    best_distance = euclidean_sum(route)
+
+    for i in range(1, len(route)-1):
+        for k in range(i+1,len(route)):
+            new_route = opt_swap_2(route[:], i, k)
+            new_distance = euclidean_sum(new_route)
+            if new_distance < best_distance:
+                best_distance = new_distance
+                route = new_route
+    return route
+
+
+def greedy2opt(node_list,allowed_time):
     start_time = time()
-    greedy_result = greedy(node_list)
+    route = greedy(node_list[:])
 
-    s = greedy_result[1]
-    z = greedy_result[0]
-    print(z)
+    improved = True
 
-    while True:
-        s_star = [node for node in s]
-        z_star = z
-        print(time() - start_time)
-        for i in range(1,len(s)):
-            for j in range(i+1,len(s)):
-                s_dash = [s_star[i],s_star[j]]
-                z_dash = euclidean_distance(s_dash[0],s_dash[1])
-                if z_dash < z_star:
-                    print(z_dash,flush=True)
-                    z_star = z_dash
-                    swap(s_star,i+1,j)
+    while improved and time() - start_time <= allowed_time:
+        s_route = route[:]
+        route = opt2(route[:])
+        if s_route == route:
+            improved = False
 
-        if s != s_star:
-            s = s_star
-            z = z_star
+    return euclidean_sum(route),route
 
-        elif s == s_star:
-            print(euclidean_sum(s_star))
-            return s_star
 
 
